9bfc13efd80831668533c7f205eb74b8cb42a9af
Leonardo Marques <l.marques@pimentagroup.de>
2017-01-20 10:38:29 +0100

    Initial Commit

---
+++ iOSAssetsResizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOSAssetsResizer
+//
+//  Created by Leonardo Marques on 20/01/2017.
+//  Copyright © 2017 LeMaMicroSystems. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
